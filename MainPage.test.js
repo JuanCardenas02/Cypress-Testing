26f937d5fc523062df05db1d824ac47cfff28fcf
JuanCardenas02 <81942652+JuanCardenas02@users.noreply.github.com>
2021-04-20 12:06:33 -0500

    Add files via upload

--- MainPage.test.js
+++ MainPage.test.js
@@ -5,6 +5,7 @@ describe ('Main Page Testing', () => {
 
     it('Correct page title', () => {
 
+        //Visiting main page
         cy.visit('https://www.vendoo.co/')
         cy.get('h1').should('have.text', 'Crossposting Made Easy!')
 
@@ -12,23 +13,19 @@ describe ('Main Page Testing', () => {
 
     it('All register buttons have correcr url', () => {
 
+        //Confirming all "Get Started" buttons have the correct url.
         cy.get('a[href="https://web.vendoo.co/register"]').should('have.attr', 'href').and('eq', 'https://web.vendoo.co/register')
 
     })
 
     it('Main header buttons', () => {
 
+        //Confirming all header buttons have correct URL.
         cy.get('a[href="/marketplaces"]').should('have.attr', 'href')
         .and('eq', '/marketplaces')
 
         cy.get('a[href="/pricing"]').should('have.attr', 'href')
         .and('eq', '/pricing')
-
-
-     //cy.get('a[href="/pricing"]').eq(1).click().url().should('eq', 'https://www.vendoo.co/pricing')
-
-    
-
 
         cy.get('a[href="https://blog.vendoo.co/"]').should('have.attr', 'href')
         .and('eq', 'https://blog.vendoo.co/')
